e6f5c1bf0146c087650f92b4168193aaa342e266
gabi <motrocgabi@gmail.com>
2026-07-23 12:36:41 +0300

    Interactive app

--- cmd/appCommands/it.go
+++ cmd/appCommands/it.go
@@ -2,8 +2,10 @@ package appCommands
 
 import (
 	"fmt"
+	"onyxide/data"
 	"os"
 
+	"charm.land/bubbles/v2/textinput"
 	tea "charm.land/bubbletea/v2"
 	"github.com/spf13/cobra"
 )
@@ -29,14 +31,15 @@ func startInteractive(cmd *cobra.Command, args []string) error {
 }
 
 func initialModel() model {
-	return model{
-		// Our to-do list is a grocery list
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+	a, _ := data.LoadApps()
 
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
+	ti := textinput.New()
+	ti.CharLimit = 156
+	ti.SetWidth(20)
+	ti.SetVirtualCursor(false)
+	return model{
+		apps:  a,
+		input: ti,
 	}
 }
 
@@ -45,79 +48,112 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
 	switch msg := msg.(type) {
 
-	// Is it a key press?
 	case tea.KeyPressMsg:
-
-		// Cool, what was the actual key pressed?
-		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q":
-			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+		if m.inputting {
+			switch msg.String() {
+			case "enter":
+				name := m.input.Value()
+				if name != "" {
+					if m.editingIndex >= 0 {
+						m.apps[m.editingIndex].Name = name
+					} else {
+						m.apps = append(m.apps, data.App{Name: name})
+						m.cursor = len(m.apps) - 1
+					}
+				}
+				m.inputting = false
+				m.editingIndex = -1
+				m.input.SetValue("")
+			case "esc":
+				m.inputting = false
+				m.input.SetValue("")
+			default:
+				var cmd tea.Cmd
+				m.input, cmd = m.input.Update(msg)
+				cmds = append(cmds, cmd)
 			}
+		} else {
+			switch msg.String() {
 
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
+			case "ctrl+c", "q":
+				return m, tea.Quit
 
-		// The "enter" key and the space bar toggle the selected state
-		// for the item that the cursor is pointing at.
-		case "enter", "space":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+
+			case "down", "j":
+				if m.cursor < len(m.apps)-1 {
+					m.cursor++
+				}
+
+			case "a":
+				m.inputting = true
+				m.input.SetValue("")
+				cmd := m.input.Focus()
+				cmds = append(cmds, cmd)
+			case "e":
+				if len(m.apps) > 0 {
+					m.inputting = true
+					m.editingIndex = m.cursor
+					m.input.SetValue(m.apps[m.cursor].Name)
+					cmd := m.input.Focus()
+					cmds = append(cmds, cmd)
+				}
+			case "s":
+				err := data.SaveApps(m.apps)
+				if err != nil {
+					return nil, nil
+				}
+				return m, tea.Quit
+
+			case "d":
+				m.apps = append(m.apps[:m.cursor], m.apps[m.cursor+1:]...)
+				if m.cursor > 0 {
+					m.cursor--
+				}
 			}
 		}
 	}
 
 	// Return the updated model to the Bubble Tea runtime for processing.
 	// Note that we're not returning a command.
-	return m, nil
+
+	//return m, nil
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m model) View() tea.View {
-	// The header
-	s := "What should we buy at the market?\n\n"
+	s := "Manage apps\n\n"
 
-	// Iterate over our choices
-	for i, choice := range m.choices {
+	for i, app := range m.apps {
 
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
+		cursor := " "
 		if m.cursor == i {
-			cursor = ">" // cursor!
+			cursor = ">"
 		}
 
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		s += fmt.Sprintf("%d) %s %s\n", i, cursor, app.Name)
 	}
 
-	// The footer
-	s += "\nPress q to quit.\n"
-
-	// Send the UI for rendering
+	if m.inputting {
+		s += "\n" + m.input.View() + "\nenter to confirm, esc to cancel"
+	} else {
+		s += "\nPress q to quit. Press a to add. Press s to save. Press d to delete\n"
+	}
 	return tea.NewView(s)
 }
 
 type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
+	apps         []data.App
+	cursor       int
+	input        textinput.Model
+	inputting    bool
+	editingIndex int
 }
